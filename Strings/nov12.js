a726454d185c274fad03a915314e7a7e0267a5f1
Spencer Pingitore <silentfoe6@gmail.com>
2022-11-12 21:41:08 -0500

    first try at figuring out the solution

---
+++ Strings/nov12.js
@@ -0,0 +1,23 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+
+//First try at working the problem
+
+function noSpace(x){
+  
+  
+    //split at every space and then join everything together with join
+
+    let answer = x.split(' ').join('')
+    
+        
+    return answer
+               
+
+}
+
+
+
+
+
