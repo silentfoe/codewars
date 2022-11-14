bc31912467c276029008c10185372f83a64db271
Spencer Pingitore <silentfoe6@gmail.com>
2022-11-14 14:04:06 -0500

    refactor for one line

--- Strings/nov14.js
+++ Strings/nov14.js
@@ -12,3 +12,9 @@ function solution(str){
     return str.split('').reverse().join('')
     
   }
+
+
+  //refactoring to have one line of code
+
+
+   const solution = str => str.split('').reverse().join('');
