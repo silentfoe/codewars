3cdf13453ce52e15e27ec8c685bf04e4eb18f626
Spencer Pingitore <silentfoe6@gmail.com>
2022-11-15 18:51:10 -0500

    first attempt at problem

---
+++ Strings/nov15.js
@@ -0,0 +1,15 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+//first attempt at the problem
+
+function removeChar(str){
+ 
+    //taking away the first character and the last by returning the string from index 1 through last index minus 1(shown by -1)
+    return str.slice(1,-1)
+  
+  };
+
+
+
+  
