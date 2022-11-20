6cf15e079a3e868fa96fc2709b10144bdfb69674
Spencer Pingitore <silentfoe6@gmail.com>
2022-11-19 19:57:51 -0500

    First attempt to solve

---
+++ Fundamentals/nov19.js
@@ -0,0 +1,27 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+// First attempt at solving
+
+function betterThanAverage(classPoints, yourPoints) {
+  
+    let school = classPoints.reduce((prev,curr) => prev + curr,0) / classPoints.length
+    
+    
+    if(school > yourPoints) {
+    
+      return false
+    }
+    
+    return true
+    
+  }
+  
