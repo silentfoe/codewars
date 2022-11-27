2da2f9d48e97548bbdfd61ad7cf2fc633d87fdc2
Spencer Pingitore <silentfoe6@gmail.com>
2022-11-26 22:23:37 -0500

    first attempt to solving problem

---
+++ Strings/nov26#2.js
@@ -0,0 +1,37 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+
+// first attempt at solving the problem
+
+
+function highAndLow(numbers){
+  
+    let largest = ''
+    let small = ''
+      
+      let arr = numbers.split(' ').sort((first, second) => first - second)
+    
+    
+      if(arr.length === 1) {
+        
+        return `${arr[0]} ${arr[0]}`
+      }
+      
+      
+      largest = arr.pop()
+      small = arr.shift()
+      
+      
+    return `${largest} ${small}`
+      
+      
+    }
