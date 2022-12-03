92bd04a69c087df60a269cf45e92da27acafefd2
Spencer Pingitore <silentfoe6@gmail.com>
2022-12-03 15:35:52 -0500

    first attempt to solve

---
+++ Fundamentals/dec3.js
@@ -0,0 +1,17 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+
+//first attempt to solve
+
+function unusualFive() {
+  
+    let five = 'five!'
+    
+    return five.length
+    
+    
+  }
+
+  
