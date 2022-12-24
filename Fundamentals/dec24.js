c81a125dd2b81cf68aa8b4d0494948ae61ea136c
Spencer Pingitore <silentfoe6@gmail.com>
2022-12-24 11:01:29 -0500

    using for loop to solve instead of reg ex

--- Fundamentals/dec24.js
+++ Fundamentals/dec24.js
@@ -18,8 +18,26 @@
 function reverseLetter(str) {
   
     return str.match(/[a-z]/g).reverse().join('')
+            
+  }
+
+
+
+//using for loop instead of reg ex
+
+  function reverseLetter(str) {
+  
+    let word = []
     
-    
+    for(let i = 0; i < str.length; i++){
+      
+    //checking to see if the character at str[i] is a letter by checking the lower case against the upper case. Will return true only in the case of a letter
+      if(str[i].toLowerCase() !== str[i].toUpperCase()){
+        //adding the letters in revese order
+        word.unshift(str[i])
+      } 
+    }
+    //joing the array to make a string
+    return word.join('')
     
   }
-  
