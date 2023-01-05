09785fff83c255bff675a96d8f980231cf46fe8e
Spencer Pingitore <silentfoe6@gmail.com>
2023-01-04 19:00:25 -0500

    first attempt to solve. Using regular expression to solve

---
+++ Fundamentals/exclamationMarksAndVowels.js
@@ -0,0 +1,20 @@
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+
+//first attempt while using regular expressions
+
+function replace(s){
+  
+  
+    return s.replace(/[a,e,i,o,u]/ig, "!")
+    
+    
+  }
+
