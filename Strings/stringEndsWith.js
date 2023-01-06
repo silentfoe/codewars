3629850bb46eb7cf51ed0153c09128bb8b2d5715
Spencer Pingitore <silentfoe6@gmail.com>
2023-01-06 16:18:02 -0500

    first attempt to solve

---
+++ Strings/stringEndsWith.js
@@ -0,0 +1,24 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//first attempt to solve
+
+function solution(str, ending){
+  
+    let len = ending.length
+    
+    let chars = str.slice(-len)
+    
+    if(ending === ''){
+      
+      return true
+    }
+    
+    return chars === ending
+    
+    
+  }
