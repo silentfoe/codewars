0fcec1ec2112218d1f9a5a9ed721bc4348c1292a
Spencer Pingitore <silentfoe6@gmail.com>
2023-01-22 23:22:45 -0500

    first attempt to solve

---
+++ Fundamentals/centuryFromYear.js
@@ -0,0 +1,24 @@
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+//my solution:
+
+function century(year) {
+  
+    if(year/100 === Math.round(year/100)){
+      return year/100
+    }else if(Math.round(year/100) === 1){
+     return 1 
+    }else{
+      return Math.floor(year/100) + 1
+    }
+  }
