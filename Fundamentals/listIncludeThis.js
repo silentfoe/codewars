375a3129ac7c6215dac238cbbff6776ac92f6d48
Spencer Pingitore <silentfoe6@gmail.com>
2023-01-25 16:08:35 -0500

    first attempt to solve

---
+++ Fundamentals/listIncludeThis.js
@@ -0,0 +1,10 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+// my solution: 
+
+function include(arr, item){
+  
+    return arr.includes(item)
+    
+  }
