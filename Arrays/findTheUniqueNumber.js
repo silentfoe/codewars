282c0f7084b48115dbead7a6293eb172d79477ca
Spencer Pingitore <silentfoe6@gmail.com>
2023-02-04 16:00:36 -0500

    solved using for loop and added comments on how I worked out my solutions

--- Arrays/findTheUniqueNumber.js
+++ Arrays/findTheUniqueNumber.js
@@ -12,8 +12,29 @@
 function findUniq(arr) {
   
     for(let i = 0; i < arr.length; i++){
+        //checking to see if the index of the number is the last place where the number is found. if it is, this must be the different number.
       if(arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])){
         return arr[i]
+      }
+    }
+  }
+  
+
+  // using for loop instead to solve
+
+function findUniq(arr) {
+    // set a point on the first number in the array
+    let num = arr[0]
+    //compare if the first number matches the last number, if it does then find the different number in between the two numbers with the find method
+    if(num === arr[arr.length - 1]){
+      return arr.find(char => char !== num)
+    }else{
+      //if the first and last number dont match, compare the first to the second number and if they match the last number must be the different number  
+      if(num === arr[1]){
+        return arr[arr.length - 1]
+      }else{
+        //if the first and the last number dont match, and the first and the second number dont match, the first number must be the different number
+        return num
       }
     }
   }
