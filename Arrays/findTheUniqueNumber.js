4ad6f4cb9ba465c9667c7a2168c452c8d74d8b3f
Spencer Pingitore <silentfoe6@gmail.com>
2023-02-04 15:55:41 -0500

    first attempt to solve using indexes to compare numbers

---
+++ Arrays/findTheUniqueNumber.js
@@ -0,0 +1,20 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+
+//my first solution using the index method: 
+
+function findUniq(arr) {
+  
+    for(let i = 0; i < arr.length; i++){
+      if(arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])){
+        return arr[i]
+      }
+    }
+  }
+  
