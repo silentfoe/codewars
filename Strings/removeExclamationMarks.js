4a884ce95c9e92ba4b82fc1a818f0e9f9e039d79
Spencer Pingitore <silentfoe6@gmail.com>
2023-02-06 17:19:20 -0500

    first attempt to solve

---
+++ Strings/removeExclamationMarks.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// my solution: 
+
+function removeExclamationMarks(s) {
+  return s.replaceAll('!','')
+}
