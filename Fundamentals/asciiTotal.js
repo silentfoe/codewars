8024e1f6ed4e2f918a2e85fbef8d36844f70b001
Spencer Pingitore <silentfoe6@gmail.com>
2023-02-19 19:56:17 -0500

    first attempt to solve

---
+++ Fundamentals/asciiTotal.js
@@ -0,0 +1,14 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+
+// Examples:
+
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+
+// my solution: 
+
+function uniTotal (string) {
+
+    return string.split('').reduce((prev,curr) => prev + curr.charCodeAt(),0)
+    
+  }
