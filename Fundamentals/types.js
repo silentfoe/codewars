152795f6fc2fe998692f7c48871e06ce6cac42db
Spencer Pingitore <silentfoe6@gmail.com>
2023-03-25 18:48:33 -0400

    first attempt to solve

---
+++ Fundamentals/types.js
@@ -0,0 +1,14 @@
+Task:
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+"42", "number" --> false
+
+// my solution: 
+
+function typeValidation(variable, type) {
+
+  return typeof variable === type
+  
+}
