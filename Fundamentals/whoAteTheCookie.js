e2ef7259ec8403f79ccd3ffad29b228ce31c1841
Spencer Pingitore <silentfoe6@gmail.com>
2023-05-03 16:44:49 -0400

    first attempt to solve

---
+++ Fundamentals/whoAteTheCookie.js
@@ -0,0 +1,23 @@
+// For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+// Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+// Note: Make sure you return the correct message with correct spaces and punctuation.
+
+// Please leave feedback for this kata. Cheers!
+
+// my solution: 
+
+function cookie(x){
+  
+  let sentence = "Who ate the last cookie? It was"
+  
+  switch(typeof x){
+      case 'string':
+        return `${sentence} Zach!`
+      case 'number':
+        return `${sentence} Monica!`
+      default: 
+        return `${sentence} the dog!`
+  }
+}
