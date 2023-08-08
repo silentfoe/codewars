e1c504be21ac6f6e7833d56bf0b4f925ad16786a
Spencer Pingitore <silentfoe6@gmail.com>
2023-08-07 20:58:11 -0400

    first attempt to solve

---
+++ Strings/exclamationMarksSeries#7.js
@@ -0,0 +1,19 @@
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+
+// my solution: 
+
+
+function remove (string) {
+  
+  return string.split(' ').filter(word => word.split('!').length !== 2).join(' ')
+  
+}
