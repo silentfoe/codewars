4e8aeb2e806604644646eef961a186250c0e1e7d
Spencer Pingitore <silentfoe6@gmail.com>
2023-10-06 22:57:22 -0400

    refactoring solutions for this problem

--- Strings/longestVowelChain.js
+++ Strings/longestVowelChain.js
@@ -17,3 +17,15 @@ function solve(s){
 
   return Math.max(...str.split('*').map(char => char.length))
 }
+
+
+//using regex to solve problem: 
+
+function solve(s){
+  return Math.max(...s.match(/[aeiou]+/gi).map(vowels => vowels.length))
+ }
+ 
+ 
+ // cool method I saw on the solutions page to solve this problem
+ 
+ const solve = s => s.split(/[^aeiou]/).reduce((s,n)=> Math.max(s,n.length),0);
