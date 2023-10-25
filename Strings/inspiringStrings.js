9af1d0ea5e6537b313fd126f9ec6c5e758a06127
Spencer Pingitore <silentfoe6@gmail.com>
2023-10-25 00:19:31 -0400

    my solution to the problem and two solutions I thought were cool to see used

--- Strings/inspiringStrings.js
+++ Strings/inspiringStrings.js
@@ -7,3 +7,24 @@
 // 'red blue gold' //returns gold
 
 // my solution:
+
+function longestWord(stringOfWords) {
+  const length = stringOfWords.split(" ").map((word) => word.length);
+  const words = stringOfWords.split(" ");
+
+  return words[length.lastIndexOf(Math.max(...length))];
+}
+
+// two cool solutions found on code wars page:
+
+//1
+let longestWord = (s) =>
+  s.split(" ").reduceRight((a, b) => (b.length > a.length ? b : a));
+
+//2
+function longestWord(str) {
+  return str
+    .split(" ")
+    .sort((b, a) => b.length - a.length)
+    .pop();
+}
