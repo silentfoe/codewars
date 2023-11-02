b88b160b40483471be701259a6549f12521a4fc9
Spencer Pingitore <silentfoe6@gmail.com>
2023-11-01 23:30:19 -0400

    finished solution for this problem.

--- Strings/neutralisation.js
+++ Strings/neutralisation.js
@@ -20,3 +20,16 @@
 // The two strings will be the same length.
 
 // my solution:
+function neutralise(s1, s2) {
+  let str = "";
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] === s2[i]) {
+      str += s1[i];
+    } else {
+      str += "0";
+    }
+  }
+
+  return str;
+}
