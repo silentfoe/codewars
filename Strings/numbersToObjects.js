f0e6f67e9c9fca7245feae12a47bc42ea1d726a8
Spencer Pingitore <silentfoe6@gmail.com>
2023-11-05 22:39:26 -0500

    first attempt to solve

--- Strings/numbersToObjects.js
+++ Strings/numbersToObjects.js
@@ -9,3 +9,22 @@
 // All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
 
 // my solution:
+
+function numObj(s) {
+  let arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let obj = {};
+    obj[s[i].toString()] = String.fromCodePoint(s[i]);
+
+    arr.push(obj);
+  }
+
+  return arr;
+}
+
+// using map to solve problem:
+
+function numObj(s) {
+  return s.map((num) => ({ [num.toString()]: String.fromCharCode(num) }));
+}
